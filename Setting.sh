b1d1c73915d96f21296588fbbe7eaecc9b46fb16
hungnguyen <nguyenthanhhung.dev@gmail.com>
2023-11-29 00:57:35 +0700

    Update boost.sh and add Setting.sh

--- Setting.sh
+++ Setting.sh
@@ -6,4 +6,7 @@ sudo dnf install libgtop2-devel lm_sensors
 sudo dnf install git
 #Installing java
 sudo dnf install java-latest-openjdk.x86_64
-
+#Installing Mysql
+sudo dnf install community-mysql-server
+#Installing nano
+sudo dnf install nano
--- boost.sh
+++ boost.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo systemctl start tuned
 sudo systemctl start cpupower.service
 sudo cpupower frequency-set -g performance
 sudo cpupower frequency-set -d 4000 all
 cpupower frequency-info
+sudo systemctl start tuned
 sudo tuned-adm active
 sudo fancontrol
