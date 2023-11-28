2322d60fa0f853669dced1aef7b02f9200fa6953
hungnguyen <nguyenthanhhung.dev@gmail.com>
2023-11-28 21:04:47 +0700

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo fancontrol
-sudo systemctl status tuned
+sudo systemctl start tuned
 sudo systemctl start cpupower.service
 sudo cpupower frequency-set -g performance
 sudo cpupower frequency-set -d 4000 all
 cpupower frequency-info
 sudo tuned-adm active
 sudo tuned-adm verify
+sudo fancontrol
