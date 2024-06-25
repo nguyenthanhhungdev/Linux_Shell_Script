cff498a63f08bee6f7c3d2061e1fefd147ecbb7e
hung <nguyenthanhhung.dev@gmail.com>
2024-06-26 01:27:33 +0700

    Bỏ cài đặt extension chỉ in ra màn hình

---
+++ create_swap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Nhận đầu vào là dung lượng swap
+SWAP_SIZE=$1
+
+# Tất cả các hàm từ `Setting_new.sh` ở đây
+# ...
+
+# Hàm tạo file swap
+create_swap() {
+    # Tạo một file swap với kích thước được chỉ định
+    sudo fallocate -l $SWAP_SIZE /swapfile
+
+    # Đặt quyền truy cập chỉ cho root
+    sudo chmod 600 /swapfile
+
+    # Đặt file này như là swap
+    sudo mkswap /swapfile
+
+    # Kích hoạt file swap
+    sudo swapon /swapfile
+
+    # Thêm file swap vào /etc/fstab để tồn tại sau khi khởi động lại
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+}
+
+# Gọi tất cả các hàm
+# ...
+
+# Tạo file swap
+create_swap
