385d60c08243bb9cba7274258696fab4237e17e5
hung <nguyenthanhhung.dev@gmail.com>
2024-06-27 04:32:17 +0700

    fix lỗi

--- setup_DE.sh
+++ setup_DE.sh
@@ -14,11 +14,13 @@ echo "Nhập số thứ tự của DE muốn cài đặt:"
 read index
 
 # Kiểm tra xem số thứ tự có hợp lệ không
-if [ "$index" -le 0 ] || [ "$index" -gt "${#de_list[@]}" ]; then
+if ! [[ "$index" =~ ^[0-9]+$ ]] || [ "$index" -le 0 ] || [ "$index" -gt "${#de_list[@]}" ]; then
   echo "Số thứ tự không hợp lệ. Thoát chương trình."
   exit 1
 fi
 
 # Cài đặt DE
 de_name=$(echo "${de_list[$((index-1))]}" | cut -d'(' -f1)
-sudo dnf group install "$de_name"
+de_name_trimmed=$(echo "$de_name" | sed 's/^ *//;s/ *$//')
+echo "Cài đặt $de_name_trimmed"
+sudo dnf group install "$de_name_trimmed"
