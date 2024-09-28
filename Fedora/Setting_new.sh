e71117a42dc6332d8fbc394fc1dd0aa4a9637fdc
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-09-28 23:03:24 +0700

    Chuyển sang dùng repo để tải vs code thay vì dùng flatpak

--- Fedora/Setting_new.sh
+++ Fedora/Setting_new.sh
@@ -165,7 +165,10 @@ install_git() {
 
 #Hàm install vscode
 install_vscode() {
-  flatpak install flathub com.visualstudio.code
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+  sudo dnf check-update
+  sudo dnf install code
 }
 
 #Hàm install Gaphor
